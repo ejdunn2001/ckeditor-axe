a6bf01be55d578afebe802e6a3f0f749a8fe577d
Azuma <azuma.sv@gmail.com>
2019-06-05 14:18:22 +0300

    Plugin architecture was improved pretty much. Configs override mechanism. AXE execution into iframe. And other small improvements.

--- axe/test.js
+++ axe/test.js
@@ -3,6 +3,9 @@
 // Add extra plugins.
 CKEDITOR.plugins.addExternal("axe", "/axe/", "plugin.js");
 CKEDITOR.config.extraPlugins = "axe,balloonpanel";
+// Configure our plugin.
+CKEDITOR.config.axe = {};
+CKEDITOR.config.axe.path = "/node_modules/axe-core/axe.js";
 // Set bigger height for testing.
 CKEDITOR.config.height = 700;
 // Set CKEditor.
---
+++ src/frame.js
@@ -0,0 +1,42 @@
+(function() {
+  /**
+   * We will add a small even listener to get signal from parent window to execute aXe check.
+   */
+  window.addEventListener("message", function(event) {
+    // @todo: We should do something to log this errors in parent window.
+    // Validate message for security.
+    if (window.origin !== event.origin) {
+      console.error("Insecure command detected.");
+      return false;
+    }
+    // Validate incoming command.
+    let msg = event.data;
+    if (typeof msg.command === "undefined") {
+      console.error("Command type is not defined.");
+      return false;
+    }
+    // Validate editor instance.
+    let CKEDITOR = window.parent.CKEDITOR;
+    if (
+      typeof msg.editor === "undefined" ||
+      typeof CKEDITOR.instances[msg.editor] === "undefined"
+    ) {
+      console.error("Editor instance is invalid.");
+      return false;
+    }
+
+    // Execute command (at current moment we have only one type of commands "run").
+    if (msg.command === "run") {
+      let args = msg.arguments;
+      let context = document.body;
+      // We will allow developer to provide custom context if they want.
+      context = typeof args.context !== "undefined" ? args.context : context;
+      // Redirect execution to parent frame.
+      axe.run(context, args.options, function(error, results) {
+        CKEDITOR.tools.callFunction(args.callback, error, results, msg.editor);
+      });
+    } else {
+      console.error("Invalid command.");
+    }
+  });
+})();
